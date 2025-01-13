5d2214fad90ce7b0d3b2fb46782a508eee4afd9c
Navneet <navneet.patwal31@gmail.com>
2025-01-13 11:13:07 +0530

    collection

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,21 +1,31 @@
 import { Link } from "expo-router";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/search";
 import { Card, FeaturedCard } from "@/components/cards";
 import Filters from "@/components/filters";
+import { useGlobalContext } from "@/lib/global-provider";
 export default function Index() {
+  const {user} = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
+      <FlatList data= {[1,2,3,4,5,6,7,8,9,10]}
+       renderItem={({item}) => ( <Card/>)} 
+       keyExtractor={(items)=> items.toString()}
+       numColumns={2}
+       contentContainerClassName="pb-32"
+       columnWrapperClassName="flex gap-5 px-5"
+       showsVerticalScrollIndicator={false}
+       ListHeaderComponent={
+        <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="felx flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <Image source={{uri: user?.avatar}} className="size-12 rounded-full"/>
             <View className="flex flex-col items-start ml-2 justify-center">
               <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-              <Text className="text-base foont-rubik-medium text-black-300">Navneet</Text>
+              <Text className="text-base foont-rubik-medium text-black-300">{user?.name}</Text>
             </View>
           </View>
           <Image source={icons.bell} className="size-6"/>
@@ -30,11 +40,15 @@ export default function Index() {
             </Text>
             </TouchableOpacity>
         </View>
-        <View className="flex flex-row gap-5 mt-5">
-          <FeaturedCard/>
-          <FeaturedCard/>
-          <FeaturedCard/>
-        </View>
+      <FlatList data= {[1,2,3,4,5,6]}
+       renderItem={({item}) => ( <FeaturedCard/>)} 
+       keyExtractor={(items)=> items.toString()}
+       horizontal
+       bounces={false}
+       showsHorizontalScrollIndicator={false}
+       contentContainerClassName="flex gap-5 mt-5"
+       className="mt-3"
+       />
       </View>
       <View className="flex flex-row items-center justify-between">
           <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
@@ -45,11 +59,10 @@ export default function Index() {
             </TouchableOpacity>
         </View>
         <Filters/>
-        <View className="flex flex-row gap-5 mt-5">
-          <Card />
-          <Card />
-        </View>
       </View> 
+       }
+       />
+      
     </SafeAreaView>
   );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,10 +1,15 @@
-import {Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import {Account, Avatars, Client, Databases, OAuthProvider } from "react-native-appwrite";
 import { openAuthSessionAsync } from "expo-web-browser";
 import * as Linking from "expo-linking";
 export const config = {
     platform : 'com.bhavans.n',
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -16,6 +21,8 @@ client.setEndpoint(config.endpoint!)
 export const avatar = new Avatars(client);
 
 export const account = new Account(client);
+
+export const databases =  new Databases(client);
 
 export const checkActiveSession = async () => {
     try {
